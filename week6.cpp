52e365be377207bb381fa6f28308a24a75fadf54
sohan765 <patelsohanlal416@gmail.com>
2026-06-17 10:27:50 +0530

    Update week6.cpp

--- week6.cpp
+++ week6.cpp
@@ -1,64 +1,64 @@
 #include<iostream>
 using namespace std;
-// class parent{
-//     public:
-//     void display()
-//     {
-//         cout<<"parent ";
-//     }
-// };
-// class child : public parent{
-//     public:
-//     //using parent::display;
-//    void display(){
-//     cout<<"parent";
-//    }
-//     void display(int)
-//     {
-//         cout<<"child";
-//     }
-// };
+class parent{
+    public:
+    void display()
+    {
+        cout<<"parent ";
+    }
+};
+class child : public parent{
+    public:
+    //using parent::display;
+   void display(){
+    cout<<"parent";
+   }
+    void display(int)
+    {
+        cout<<"child";
+    }
+};
 
-    // int *p;
-    // int x = 10;
-    // int y;
-    // p = &x;
-    // y = *p;
-    // *p = 20;
-    // &x = p;
-    // child c;
-    // c.display();
-// static volatile int i;
-// void fun(void){
-//     i=0;
-//     do{
-//         cout<<"the value of i is "<<i<<endl;
+    int *p;
+    int x = 10;
+    int y;
+    p = &x;
+    y = *p;
+    *p = 20;
+    &x = p;
+    child c;
+    c.display();
+static volatile int i;
+void fun(void){
+    i=0;
+    do{
+        cout<<"the value of i is "<<i<<endl;
         
-//     }while(i!=100);
+    }while(i!=100);
 
-// }
-// void function(int&,int);
-// int function(const int &x)
-// {
-//     return (x+1);
-//     /
-// }
-// int function(int x=25,int y)
-// {
-//     cout<<"x="<<x<<"y="<<y<<endl;
+}
+void function(int&,int);
+int function(const int &x)
+{
+    return (x+1);
+    /
+}
+int function(int x=25,int y)
+{
+    cout<<"x="<<x<<"y="<<y<<endl;
 
-// }
-// int main()
-// {
-//     // int a =10,b;
-//     // b = function(a);
-//     // cout<<"a="<<a<<"&a="<<&a<<endl;
-//     // cout<<b<<endl;
-//     int a,b;
-//     function(10);
+}
+int main()
+{
+    // int a =10,b;
+    // b = function(a);
+    // cout<<"a="<<a<<"&a="<<&a<<endl;
+    // cout<<b<<endl;
+    int a,b;
+    function(10);
 
-//     return 0;
-// }
+    return 0;
+}
 int add(int a,int b)
 {
     cout<<a+b<<endl;
