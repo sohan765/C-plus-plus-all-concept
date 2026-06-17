79dd36ac5a8dc89680a29f86c1dedfa7664b9171
sohan765 <patelsohanlal416@gmail.com>
2026-06-17 10:31:29 +0530

    Update infixx.cpp

--- infixx.cpp
+++ infixx.cpp
@@ -2,42 +2,42 @@
 #include<cstring>
 #include<stack>
 using namespace std;
-// typedef struct stack{
-//     char data[20];
-//     int top;
+typedef struct stack{
+    char data[20];
+    int top;
 
-// }stack;
-// int empty(stack *p)
-// {
-//     return (p->top == -1);
-// }
-// int top(stack *p)
-// {
-//     return (p->data[p->top]);
-// }
-// void push(stack *p,char x)
-// {
-//     p->data[++(p->top)] = x;
-// }
-// void pop(stack *p)
-// {
- //     if(!empty(p))
-//     (p->top)=-1;
- // }
+}stack;
+int empty(stack *p)
+{
+    return (p->top == -1);
+}
+int top(stack *p)
+{
+    return (p->data[p->top]);
+}
+void push(stack *p,char x)
+{
+    p->data[++(p->top)] = x;
+}
+void pop(stack *p)
+{
+     if(!empty(p))
+    (p->top)=-1;
+ }
  int main()
 {
-//     char str[10] = "ABCDEG";
-//     stack<char> s;
-//     for(int i = 0; i<strlen(str);i++)
-//     {
-//         s.push(str[i]);
+    char str[10] = "ABCDEG";
+    stack<char> s;
+    for(int i = 0; i<strlen(str);i++)
+    {
+        s.push(str[i]);
         
-//     }
-//     cout<<"reversed string "<<"  ";
-//     while(!s.empty()){
-//         cout<<s.top();
-//         s.pop();
-//     }
+    }
+    cout<<"reversed string "<<"  ";
+    while(!s.empty()){
+        cout<<s.top();
+        s.pop();
+    }
 char postfix[] = { '1','2','3','*','+','4','-' },ch;
 stack<int> s;
 for(int i=0;i<7;i++)
